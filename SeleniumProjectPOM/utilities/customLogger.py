e6230bca2c81825e9f57836f19445293a69730d9
AmadouDiallo4 <diallobis2011@gmail.com>
2023-07-08 15:35:13 +0200

    update loggin/html reports code

--- SeleniumProjectPOM/utilities/customLogger.py
+++ SeleniumProjectPOM/utilities/customLogger.py
@@ -5,11 +5,15 @@ class LogGen:
 
     @staticmethod
     def loggen():
-        logging.basicConfig(filename=r".\\Logs\\automation.log",
-                            encoding='utf-8',
-                        format='%(asctime)s: %(levelname)s: %(message)s',
-                        datefmt='%m%d%Y %I:%M:%S %p')
+        #logging.basicConfig(filename=".\\automation.log",
+        #                   encoding='utf-8',
+        #              format='%(asctime)s: %(levelname)s: %(message)s',
+        #                datefmt='%m%d%Y %I:%M:%S %p')
+        fileHandler = logging.FileHandler('\\automation.log')
+        formatter = logging.Formatter("%(asctime)s: %(levelname)s: %(name)s :%(message)s")
+        fileHandler.setFormatter(formatter)
 
         logger=logging.getLogger()
+        logger.addHandler(fileHandler)
         logger.setLevel(logging.INFO)
         return logger
